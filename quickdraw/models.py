535ac23e13983d53921717ec2bab09447add5b83
sanghunKang <sanghunkang.dev@gmail.com>
2017-08-20 19:46:33 +0900

    qd:3fc, cd: 64->32

--- quickdraw/models.py
+++ quickdraw/models.py
@@ -39,25 +39,25 @@ class SubmissionModel(BaseModel):
 		net = model_input
 		print(net.shape)
 
-		net = slim.conv2d(net, 64, [3, 3], scope='conv11')#, weights_regularizer=slim.l2_regularizer(l2_penalty))
+		net = slim.conv2d(net, 32, [3, 3], scope='conv11')#, weights_regularizer=slim.l2_regularizer(l2_penalty))
 		net = slim.max_pool2d(net, [2, 2], scope='pool11')
 		print(net.shape)
 
-		net = slim.conv2d(net, 128, [3, 3], scope='conv12')#, weights_regularizer=slim.l2_regularizer(l2_penalty))
+		net = slim.conv2d(net, 64, [3, 3], scope='conv12')#, weights_regularizer=slim.l2_regularizer(l2_penalty))
 		net = slim.max_pool2d(net, [2, 2], scope='pool12')
 		print(net.shape)
 
-		net = slim.conv2d(net, 256, [3, 3], scope='conv21')#, weights_regularizer=slim.l2_regularizer(l2_penalty))
+		net = slim.conv2d(net, 128, [3, 3], scope='conv21')#, weights_regularizer=slim.l2_regularizer(l2_penalty))
 		net = slim.max_pool2d(net, [2, 2], scope='pool21')
 		print(net.shape)
 
-		net = slim.conv2d(net, 512, [3, 3], scope='conv22')#, weights_regularizer=slim.l2_regularizer(l2_penalty))
+		net = slim.conv2d(net, 256, [3, 3], scope='conv22')#, weights_regularizer=slim.l2_regularizer(l2_penalty))
 		net = slim.max_pool2d(net, [2, 2], scope='pool22')
 		print(net.shape)
 
-		# net = slim.conv2d(net, 512, [3, 3], scope='conv31')#, weights_regularizer=slim.l2_regularizer(l2_penalty))
-		# net = slim.max_pool2d(net, [3, 3], scope='pool31')
-		# print(net.shape)
+		net = slim.conv2d(net, 512, [3, 3], scope='conv31')#, weights_regularizer=slim.l2_regularizer(l2_penalty))
+		net = slim.max_pool2d(net, [3, 3], scope='pool31')
+		print(net.shape)
 
 		# net = slim.conv2d(net, 128, [3, 3], scope='conv32')#, weights_regularizer=slim.l2_regularizer(l2_penalty))
 		# net = slim.max_pool2d(net, [2, 2], scope='pool32')
@@ -79,6 +79,9 @@ class SubmissionModel(BaseModel):
 
 		net = slim.flatten(net)
 		# net = slim.batch_norm(net, scope='bn1')
+		net = slim.dropout(net, 0.5, is_training=is_training, scope='dropout0')
+		net = slim.fully_connected(net, int(net.shape[-1]), activation_fn=tf.nn.relu, weights_regularizer=slim.l2_regularizer(l2_penalty), scope='fc0')
+
 		net = slim.dropout(net, 0.5, is_training=is_training, scope='dropout1')
 		net = slim.fully_connected(net, int(net.shape[-1]), activation_fn=tf.nn.relu, weights_regularizer=slim.l2_regularizer(l2_penalty), scope='fc1')
 		# net = slim.batch_norm(net, scope='bn2')
